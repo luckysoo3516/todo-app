4b4e64bb2a5dc45e174b5f939c13ac312f244fd3
luckysoo3516 <sumin328030@gmail.com>
2020-03-14 19:43:53 +0900

    state를 바꾸는 이벤트를 생성하였음.
    button을 클릭하면 상태를 업데이트하는 함수를 만들었다.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ import List from './List.jsx';
 
 function App() {
   const [todos, setTodos] = useState(['공지사항 만들기']);
+  const [newTodo, setNewTodo] = useState();
+  const changeInputData = (e) => {
+    setNewTodo(e.target.value);
+  }
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+  }
   return (
     <>
     <h1>
       Todo 애플리케이션
     </h1>
     <form action="">
-      <input type="text" name=""></input>
-      <button>할일 추가</button>
+      <input type="text" name="" onChange={changeInputData}></input>
+      <button onClick={addTodo}>할일 추가</button>
     </form>
     <List todos={todos}/>
     </>
